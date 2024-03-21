a82d3eaffafe9a964803a71e15c2a4574c85cfa2
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-03-21 21:32:33 +0300

    update dependencies

--- src/main/react/yalniz-app/vitest.setup.mjs
+++ src/main/react/yalniz-app/vitest.setup.mjs
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/vitest';
-import { vi } from 'vitest';
+import {vi} from 'vitest';
 
 const { getComputedStyle } = window;
 window.getComputedStyle = (elt) => getComputedStyle(elt);

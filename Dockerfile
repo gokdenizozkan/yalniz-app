6cf79644715ad2d3fd0242a942330bf6f7f5d919
Gökdeniz Özkan <gokdeniz@gokdenizozkan.com>
2024-03-21 13:08:27 +0300

    adjust Database url

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:21-jre
 WORKDIR /app
-COPY build/*.jar /app.jar
+COPY target/*.jar /app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/react/yalniz-app/src/config.ts
+++ src/main/react/yalniz-app/src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://localhost:8080/";
+export const API_BASE_URL = "https://yalnizapp-1-0-0.onrender.com/";
 export const API_URL = API_BASE_URL + "api/v2/";
